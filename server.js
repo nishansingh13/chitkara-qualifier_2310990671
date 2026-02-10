a52e54b5934e4d040ca67a89472d98d4f658ddef
Nishan Singh <nishansingh2480@gmail.com>
2026-02-10 12:27:05 +0530

    add 404 error handling

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-
+const EMAIL = "nishan0671.be23@chitkara.edu.in";
 import express from "express";
 import { healthRouter } from "./routes/health.js";
 import { operationsRouter } from "./routes/operations.js";
@@ -8,6 +8,14 @@ const app = express();
 app.use(express.json());
 app.use("/health", healthRouter);
 app.use("/bfhl", operationsRouter);
+app.use((req, res) => {
+  res.status(404).json({
+    is_success: false,
+    official_email: EMAIL,
+    error: "Endpoint not found"
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
